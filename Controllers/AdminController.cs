67cf3a6a01edb44e35da47d62f2c59a460bc6f10
tello911 <65243549+tello911@users.noreply.github.com>
2021-05-25 23:19:13 +0800

    Part 6: Admin View
    a little bit difference of external css, js compare to video..
    Implementation of getting data from DB..
    
    Uses of Controller to create View and output Data from the Model..
    
    Build solution before release to the web..
    
    Understanding of RenderBody()

---
+++ Controllers/AdminController.cs
@@ -0,0 +1,26 @@
+﻿using ABC_Tutorial.DAL;
+using ABC_Tutorial.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ABC_Tutorial.Controllers
+{
+    public class AdminController : Controller
+    {
+        // GET: Admin
+        public GenericUnitOfWork _unitOfWork = new GenericUnitOfWork();
+        public ActionResult Dashboard()
+        {
+            return View();
+        }
+
+        public ActionResult Categories()
+        {
+            List<Tbl_Category> allcategories = _unitOfWork.GetRepositoryInstance<Tbl_Category>().GetAllRecordsIQueryable().Where(i => i.IsDelete == false).ToList();
+            return View(allcategories);
+        }
+    }
+}
